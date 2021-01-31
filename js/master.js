9752077d578781556dab6e75902ed83a6c888431
mkirkpatrick802 <33790548+mkirkpatrick802@users.noreply.github.com>
2021-01-31 15:05:34 -0500

    Delete js directory

--- js/master.js
+++
@@ -1,9 +0,0 @@
-function Menu(x) {
-  x.classList.toggle("change");
-  var drop = document.getElementById("myDropdown");
-  if (drop.style.display === "block") {
-    drop.style.display = "none";
-  } else {
-    drop.style.display = "block";
-  }
-}
