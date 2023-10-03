4451e195e77528a810df38c50c386ca86266cbc7
Michael Kirkpatrick <michael.kirkpatrick@mymail.champlain.edu>
2023-10-03 02:16:58 -0400

    Redesign

--- js/master.js
+++ js/master.js
@@ -18,3 +18,45 @@ function ShowAndHide() {
         x.style.display = 'none';
     }
 }
+
+const project = document.querySelectorAll(".project");
+const blackout = document.querySelector(".BlackOut");
+isClicked = false;
+
+project.forEach(item =>{
+  item.addEventListener("mouseenter", () =>{
+    item.querySelector("video").play();
+    item.querySelector(".text").classList.remove("active");
+  })
+});
+
+project.forEach(item =>{
+  item.addEventListener("mouseleave", () =>{
+    if (!isClicked)
+    {
+      item.querySelector("video").pause();
+      item.querySelector(".text").classList.add("active");
+    }
+  })
+});
+
+project.forEach(item =>{
+  item.addEventListener("click", () =>{
+    if(!isClicked)
+    {
+      item.querySelector("video").classList.add("sized")
+      item.querySelector("video").play();
+      item.querySelector(".text").classList.remove("active");
+      blackout.classList.add("active");
+    }
+    else
+    {
+      item.querySelector("video").classList.remove("sized")
+      item.querySelector("video").pause();
+      item.querySelector(".text").classList.add("active");
+      blackout.classList.remove("active");
+    }
+
+    isClicked = !isClicked;
+  })
+});
