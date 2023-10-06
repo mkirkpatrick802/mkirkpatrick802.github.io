e9dbc9b5fa0c20cabe3bd9a89f8c1bd7fb6ee6dd
Michael Kirkpatrick <mkirkpatrick802@gmail.com>
2023-10-05 20:58:07 -0400

    It finally looks decent

--- js/master.js
+++ js/master.js
@@ -1,27 +1,11 @@
-const filler = document.querySelector(".ProjectFiller");
-
 function Menu(x) {
   x.classList.toggle("change");
   var drop = document.getElementById("myDropdown");
-  if (drop.style.display === "block")
-  {
+  if (drop.style.display === "block") {
     drop.style.display = "none";
-  }
-  else
-  {
+  } else {
     drop.style.display = "block";
   }
-}
-
-function ShowAndHide() {
-    var y = document.getElementById('SecretButton')
-    var x = document.getElementById('Secret');
-    if (x.style.display == 'none') {
-        x.style.display = 'block';
-        y.style.display = 'none';
-    } else {
-        x.style.display = 'none';
-    }
 }
 
 const project = document.querySelectorAll(".project");
