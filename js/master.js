3825a51aecb84bb419d1a2081c88a1f2b9c81705
Michael Kirkpatrick <mkirkpatrick802@gmail.com>
2023-10-12 11:50:09 -0400

    Resume Content & Fixed Javascript

--- js/master.js
+++ js/master.js
@@ -10,12 +10,16 @@ function Menu(x) {
 
 const project = document.querySelectorAll(".project");
 const blackout = document.querySelector(".BlackOut");
+let clickedProject;
 isClicked = false;
 
 project.forEach(item =>{
   item.addEventListener("mouseenter", () =>{
-    item.querySelector("video").play();
-    item.querySelector(".text").classList.remove("active");
+    if (!blackout.classList.contains("active"))
+    {
+      item.querySelector("video").play();
+      item.querySelector(".text").classList.remove("active");
+    }
   })
 });
 
@@ -31,21 +35,29 @@ project.forEach(item =>{
 
 project.forEach(item =>{
   item.addEventListener("click", () =>{
-    if(!isClicked)
-    {
-      item.querySelector("video").classList.add("sized")
-      item.querySelector("video").play();
-      item.querySelector(".text").classList.remove("active");
-      blackout.classList.add("active");
-    }
-    else
-    {
-      item.querySelector("video").classList.remove("sized")
-      item.querySelector("video").pause();
-      item.querySelector(".text").classList.add("active");
-      blackout.classList.remove("active");
-    }
+    if (isClicked) return;
 
-    isClicked = !isClicked;
+    clickedProject = item;
+    clickedProject.querySelector("video").classList.add("sized");
+    clickedProject.querySelector("video").play();
+    clickedProject.querySelector(".text").classList.remove("active");
+    clickedProject.style.pointerEvents = "none";
+
+    blackout.classList.add("active");
+    blackout.style.pointerEvents = "all";
+    blackout.style.cursor = "pointer";
+    isClicked = true;
   })
 });
+
+blackout.addEventListener("click", () =>{
+  clickedProject.querySelector("video").classList.remove("sized");
+  clickedProject.querySelector("video").pause();
+  clickedProject.querySelector(".text").classList.add("active");
+  clickedProject.style.pointerEvents = "all";
+
+  blackout.classList.remove("active");
+  blackout.style.pointerEvents = "none";
+  blackout.style.cursor = "default";
+  isClicked = false;
+})
